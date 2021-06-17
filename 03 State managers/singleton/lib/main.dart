427b091c67b219b41fa74357f25ed2da1a442c2b
Reynaldo GT <rael.thasss@gmail.com>
2021-06-17 15:04:54 -0400

    diseño listo para la implementacion de patrones de diseños

---
+++ 03 State managers/singleton/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:singleton/pages/pagina2_page.dart';
+import 'package:singleton/pages/pagina1_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'pagina1',
+        routes: {
+          'pagina1': (_) => Pagina1Page(),
+          'pagina2': (_) => Pagina2Page(),
+        });
+  }
+}
